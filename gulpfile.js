4c59ebf882290eaef32def6618b365aaec40cd02
Andrew McPherson <andrewmcp333@gmail.com>
2015-06-15 18:38:48 -0400

    Configured the project with a gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp")
+var gulp_connect = require("gulp-connect")
+
+var opn = require("opn")
+var del = require("del")
+
+gulp.task("default", function() {
+    gulp.start("build")
+})
+
+gulp.task("build", function() {
+    gulp.src("./source/**/*")
+        .pipe(gulp.dest("./build"))
+        .pipe(gulp_connect.reload())
+})
+
+gulp.task("watch", function() {
+    gulp.start("build")
+    gulp.watch("./source/**/*", function() {
+        gulp.start("build")
+    })
+})
+
+gulp.task("server", function() {
+    gulp.start("watch")
+    gulp_connect.server({
+		port: 8080,
+		root: "./build",
+		livereload: true,
+	})
+    opn("http://localhost:8080")
+})
