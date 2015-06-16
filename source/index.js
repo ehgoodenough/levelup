7b282707e33370bed759cee7445f87445706371f
Andrew McPherson <andrewmcp333@gmail.com>
2015-06-15 20:16:09 -0400

    Continued to pull my hair out over math.

--- source/index.js
+++ source/index.js
@@ -1,7 +1,12 @@
 var WIDTH = 640
 var HEIGHT = 320
 
-var basetimestamp = Date.now()
+var oldtimestamp = Date.now()
+
+var point = {x: 0, y: HEIGHT / 2, vy: 0}
+var accelerations = []
+
+//http://www.freescale.com/files/sensors/doc/app_note/AN3397.pdf
 
 $(document).ready(function() {
     $("canvas").attr("width", WIDTH)
@@ -10,17 +15,52 @@ $(document).ready(function() {
     var canvas = $("canvas")[0].getContext("2d")
     
     canvas.beginPath()
-    canvas.moveTo(0, HEIGHT / 2)
+    canvas.moveTo(point.x, point.y)
     
     if(window.DeviceMotionEvent) {
         window.addEventListener("devicemotion", function(event) {
-            var acceleration = event.accelerationIncludingGravity
-            var timestamp = event.timeStamp
             
-            var x = (timestamp - basetimestamp) * (1/32)
-            var y = (HEIGHT / 2) - acceleration.y * 8
+            if(event.acceleration.x == null
+            && event.acceleration.y == null
+            && event.acceleration.z == null) {
+                return
+            }
             
-            canvas.lineTo(x, y)
+            accelerations.push(event.acceleration)
+            if(accelerations.length > 20) {
+                accelerations.shift()
+            }
+            
+            acceleration = {}
+            acceleration.x = 0
+            acceleration.y = 0
+            acceleration.z = 0
+            for(var i = 0; i < accelerations.length; i++) {
+                acceleration.x += accelerations[i].x
+                acceleration.y += accelerations[i].y
+                acceleration.z += accelerations[i].z
+            }
+            acceleration.x /= accelerations.length
+            acceleration.y /= accelerations.length
+            acceleration.z /= accelerations.length
+            
+            // "mechanical filtering"
+            if(acceleration.y < 0.2
+            && acceleration.y > -0.2) {
+                acceleration.y = 0
+            }
+            
+            point.vy += acceleration.y
+            
+            // "move end check"
+            if(acceleration.y == 0) {
+                point.vy = 0
+            }
+            
+            point.x += 10 * event.interval
+            point.y += point.vy
+            
+            canvas.lineTo(point.x, point.y)
             canvas.stroke()
         }, false)
     } else {
