51e344764e7c769cb1680161362712057600057b
Andrew McPherson <andrewmcp333@gmail.com>
2015-06-16 14:58:37 -0400

    Attempted to mock acceleration with the keyboard.

--- source/index.js
+++ source/index.js
@@ -1,40 +1,127 @@
-var WIDTH = 640
-var HEIGHT = 320
-
 //http://www.freescale.com/files/sensors/doc/app_note/AN3397.pdf
+
+window.WIDTH = 640
+window.HEIGHT = 320
+
+var keys = {
+    // !!
+}
+
+var point = {
+    "x": 0,
+    "y": HEIGHT / 2,
+    "vx": 10,
+    "vy": 0,
+    "ax": 0,
+    "ay": 0
+}
 
 $(document).ready(function() {
     $("canvas").attr("width", WIDTH)
     $("canvas").attr("height", HEIGHT)
-    
-    var curler = new Curler(0.1)
-    
+
     var canvas = $("canvas")[0].getContext("2d")
+
     canvas.beginPath()
-    
-    for(var i = 0; i < data.length; i++) {
-        
-        if(curler.isCurl(data[i])) {
-            canvas.fillStyle = "red"
-            canvas.fillRect(x, y-20, 1, 20)
+    canvas.moveTo(point.x, point.y)
+
+    window.runLoop(function(interval) {
+        $("div").text(interval)
+
+        // x
+        point.x += point.vx * interval
+
+        // y
+        if(keys[38] || keys[87]) {
+            point.ay -= 10 * interval
+        } else if(keys[40] || keys[83]) {
+            point.ay += 10 * interval
+        } else {
+            if(point.ay > 0) {
+                point.ay -= 10 * interval
+            } else if(point.ay < 0) {
+                point.ay += 10 * interval
+            }
         }
-        
-        var x = (data[i].timestamp - data[0].timestamp) * 50
-        var y = -60 - data[i].ty * 300
-        canvas.strokeStyle = "black"
-        canvas.lineTo(x, y)
+
+        if(point.ay == 0) {
+            point.vy = 0
+        }
+
+        point.vy += point.ay * interval
+        point.y += point.vy * interval
+
+        canvas.lineTo(point.x, point.y)
         canvas.stroke()
-    }
+    })
+
+    window.addEventListener("keydown", function(event) {
+        keys[event.keyCode] = true
+    })
+
+    window.addEventListener("keyup", function(event) {
+        delete keys[event.keyCode]
+    })
 })
 
+window.runLoop = function(func) {
+    (function loop(time) {
+        func(Math.min((Date.now() - time) / 1000, 1))
+        window.requestAnimationFrame(loop.bind(null, Date.now()))
+    })(Date.now())
+}
+
+/*$(document).ready(function() {
+    $("canvas").attr("width", WIDTH)
+    $("canvas").attr("height", HEIGHT)
+
+    var canvas = $("canvas")[0].getContext("2d")
+
+    canvas.beginPath()
+    canvas.moveTo(point.x, point.y)
+
+    window.addEventListener("devicemotion", function(event) {
+
+        if(event.acceleration.x == null
+        && event.acceleration.y == null
+        && event.acceleration.z == null) {
+            return
+        }
+
+        point.ax = event.acceleration.x
+        point.ay = event.acceleration.y
+        var interval = event.interval
+
+        // "mechanical filtering"
+        if(point.ay < 0.2
+        && point.ay > -0.2) {
+            point.ay = 0
+        }
+
+        // "move end check"
+        if(point.ay == 0) {
+            point.vy = 0
+        } else {
+            point.vy += point.ay * interval
+        }
+
+        point.x += 10 * interval
+        point.vy += point.ay * interval
+        point.y += point.vy * interval * 100
+
+        canvas.lineTo(point.x, point.y)
+        canvas.stroke()
+    }, false)
+})*/
+
 var Curler = function(curldist) {
-    this.curldist = curldist || 0.1
-    
+
     this.slope = 0
     this.peakpoint = 0
     this.troughpoint = 0
     this.pointqueue = new Array()
-    
+    this.curldist = curldist || 0.1
+
     this.isCurl = function(point) {
         var isCurl = false
         var prevpoint = this.pointqueue[this.pointqueue.length - 1]
