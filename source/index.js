2c8a60b4358585ea593d025142ee3d4c8d03793b
Andrew McPherson <andrewmcp333@gmail.com>
2015-06-15 20:29:39 -0400

    Plotted the precaptured data from tango.

--- source/index.js
+++ source/index.js
@@ -1,11 +1,6 @@
 var WIDTH = 640
 var HEIGHT = 320
 
-var oldtimestamp = Date.now()
-
-var point = {x: 0, y: HEIGHT / 2, vy: 0}
-var accelerations = []
-
 //http://www.freescale.com/files/sensors/doc/app_note/AN3397.pdf
 
 $(document).ready(function() {
@@ -15,56 +10,10 @@ $(document).ready(function() {
     var canvas = $("canvas")[0].getContext("2d")
     
     canvas.beginPath()
-    canvas.moveTo(point.x, point.y)
-    
-    if(window.DeviceMotionEvent) {
-        window.addEventListener("devicemotion", function(event) {
-            
-            if(event.acceleration.x == null
-            && event.acceleration.y == null
-            && event.acceleration.z == null) {
-                return
-            }
-            
-            accelerations.push(event.acceleration)
-            if(accelerations.length > 20) {
-                accelerations.shift()
-            }
-            
-            acceleration = {}
-            acceleration.x = 0
-            acceleration.y = 0
-            acceleration.z = 0
-            for(var i = 0; i < accelerations.length; i++) {
-                acceleration.x += accelerations[i].x
-                acceleration.y += accelerations[i].y
-                acceleration.z += accelerations[i].z
-            }
-            acceleration.x /= accelerations.length
-            acceleration.y /= accelerations.length
-            acceleration.z /= accelerations.length
-            
-            // "mechanical filtering"
-            if(acceleration.y < 0.2
-            && acceleration.y > -0.2) {
-                acceleration.y = 0
-            }
-            
-            point.vy += acceleration.y
-            
-            // "move end check"
-            if(acceleration.y == 0) {
-                point.vy = 0
-            }
-            
-            point.x += 10 * event.interval
-            point.y += point.vy
-            
-            canvas.lineTo(point.x, point.y)
-            canvas.stroke()
-        }, false)
-    } else {
-        console.log("DeviceMotion is not supported.")
-        window.alert("DeviceMotion is not supported.")
+    for(var i = 0; i < data.length; i++) {
+        var x = (data[i].timestamp - data[0].timestamp) * 50
+        var y = -60 - data[i].ty * 300
+        canvas.lineTo(x, y)
+        canvas.stroke()
     }
 })
