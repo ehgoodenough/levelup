866008137ac408218dc6b0152a8da1fac339d3ad
Andrew McPherson <andrewmcp333@gmail.com>
2015-07-09 15:54:19 -0400

    Counting the curls!! :D

--- source/index.js
+++ source/index.js
@@ -2,9 +2,10 @@ var firebase = new Firebase("https://protolevelup.firebaseio.com/orientation")
 
 firebase.on("child_added", function(data) {
     var value = data.val()
-    
+
     $("#absolute").text(value.absolute == true)
     $("#alpha").text(value.alpha)
     $("#beta").text(value.beta)
     $("#gamma").text(value.gamma)
+    $("#curls").text(value.curls)
 })
--- source/sensors.js
+++ source/sensors.js
@@ -2,15 +2,37 @@ var firebase = new Firebase("https://protolevelup.firebaseio.com/orientation")
 
 firebase.remove()
 
+var trough = 10
+var peak = 80
+
+var goingUp = true
+var curls = 0
+
 window.addEventListener("deviceorientation", function(event) {
     $("#absolute").text(event.absolute == true)
-    $("#alpha").text(event.alpha)
     $("#beta").text(event.beta)
     $("#gamma").text(event.gamma)
-    
+    $("#alpha").text(event.alpha)
+
+
+    var gamma = Math.abs(event.gamma)
+    if(goingUp) {
+        if(gamma >= peak) {
+            goingUp = false
+            curls += 1
+        }
+    } else {
+        if(gamma <= trough) {
+            goingUp = true
+        }
+    }
+
+    $("#curls").text(curls)
+
     firebase.push({
         "alpha": event.alpha,
         "beta": event.beta,
-        "gamma": event.gamma
+        "gamma": event.gamma,
+        "curls": curls
     })
 }, false)
