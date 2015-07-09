9695e6823d33cc12aee7ada8a79146ff8a3c5945
Andrew McPherson <andrewmcp333@gmail.com>
2015-07-09 16:34:04 -0400

    Cleaned up the index.

--- source/index.js
+++ source/index.js
@@ -1,11 +1,19 @@
 var firebase = new Firebase("https://protolevelup.firebaseio.com/orientation")
 
+firebase.remove()
+
 firebase.on("child_added", function(data) {
     var value = data.val()
+    $("#gamma").text(value.gamma.toFixed(2))
     console.log(value)
-    $("#absolute").text(value.absolute == true)
-    $("#alpha").text(value.alpha)
-    $("#beta").text(value.beta)
-    $("#gamma").text(value.gamma)
-    $("#curls").text(value.curls)
 })
+
+
+window.addEventListener("deviceorientation", function(event) {
+    var orientation = {
+        "beta": event.beta,
+        "gamma": event.gamma,
+        "alpha": event.alpha
+    }
+    firebase.push(orientation)
+}, false)
